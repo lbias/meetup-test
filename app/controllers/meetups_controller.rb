bc6043df8678d5d4fcfd2cbb1ac57d6461ad7fdd
lbias <306508354@qq.com>
2017-05-13 13:01:58 +0800

    meetups show mehod

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -13,6 +13,10 @@ class MeetupsController < ApplicationController
     redirect_to meetups_path
   end
 
+  def show
+    @meetup = Meetup.find(params[:id])
+  end
+  
   private
 
   def meetup_params
