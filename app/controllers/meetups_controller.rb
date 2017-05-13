7de69cfa63417c224540f14feefd5f3c29ed3b0b
lbias <306508354@qq.com>
2017-05-13 13:00:12 +0800

    meetups create method

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -2,7 +2,20 @@ class MeetupsController < ApplicationController
   def index
     @meetups = Meetup.all
   end
+
   def new
     @meetup = Meetup.new
-  end  
+  end
+
+  def create
+    @meetup = Meetup.new(meetup_params)
+    @meetup.save
+    redirect_to meetups_path
+  end
+
+  private
+
+  def meetup_params
+    params.require(:meetup).permit(:title, :description)
+  end
 end
