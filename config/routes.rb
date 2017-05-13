e8dbd41d2ccfcbef4b6e2666aa8b322987e87682
lbias <306508354@qq.com>
2017-05-13 16:59:08 +0800

    routes for api/vi users

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
     resources :comments
   end
   root "meetups#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
+    
 end
