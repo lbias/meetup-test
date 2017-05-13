e3211e2f7752444b78aeeaf90a6bc486eaf0b7df
lbias <306508354@qq.com>
2017-05-13 17:38:51 +0800

    remove def api_error(opts = {}) in api/v1/sessions_controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,10 +8,6 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
-  def api_error(opts = {})
-    render nothing: true, status: opts[:status]
-  end
-
   private
 
   def create_params
